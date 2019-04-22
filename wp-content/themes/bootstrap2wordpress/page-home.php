6703baf6d71cfc6f373b24fb52c460a152f2e503
zoltan <zoltan_bogdan@live.com>
2019-04-22 16:45:35 +0200

    adding page-home custom fields and update page

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -3,7 +3,18 @@
  Template Name: Home Page
  */
 
-get_header(); ?><!-- HERO -->
+// Custom fields
+$prelaunch_price    = get_post_meta( 9, 'prelaunch_price', true);
+$launch_price    = get_post_meta( 9, 'launch_price', true);
+$final_price    = get_post_meta( 9, 'final_price', true);
+$course_url    = get_post_meta( 9, 'course_url', true);
+$button_text    = get_post_meta( 9, 'button_text', true);
+$optin_text    = get_post_meta( 9, 'optin_text', true);
+$optin_button_text    = get_post_meta( 9, 'optin_button_text', true);
+
+get_header(); ?>
+
+<!-- HERO -->
 <section id="hero" data-type="background" data-speed="5">
 	<article>
 		<div class="container clearfix">
@@ -12,31 +23,30 @@ get_header(); ?><!-- HERO -->
 					<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png" alt="Bootstrap to Wordpress" class="logo">
 				</div><!-- col -->
 				<div class="col-sm-7 hero-text">
-					<h1>Bootstrap to Wordpress</h1>
-					<p class="lead">Earn an extra $1k - $5k a Month by Learning to Code your very own responsive
-						&amp; Custom Wordpress websites with Bootstrap</p>
+					<h1><?php bloginfo('name')?></h1>
+					<p class="lead"><?php bloginfo('description')?></p>
 
 					<div id="price-timeline">
 						<div class="price active">
-							<h4>Pre-launch Price <small>Ends soon</small></h4>
-							<span>149$</span>
+							<h4>Pre-Launch Price<small>Ends soon</small></h4>
+							<span><?php echo $prelaunch_price ?></span>
 						</div>
 						<!--price-->
 						<div class="price">
 							<h4>Launch Price <small>Coming soon</small></h4>
-							<span>299$</span>
+							<span><?php echo $launch_price ?></span>
 						</div>
 						<!--price-->
 						<div class="price">
 							<h4>Final Price <small>Coming soon</small></h4>
-							<span>399$</span>
+							<span><?php echo $final_price ?></span>
 						</div>
 						<!--price-->
 
 					</div>
 					<!--price-timeline-->
 
-					<p><a class="btn btn-lg btn-danger" href="/" role="button">Enroll &raquo;</a></p>
+					<p><a class="btn btn-lg btn-danger" href="<?php echo $course_url?>" role="button"><?php echo $button_text?></a></p>
 
 				</div>
 				<!--col-->
@@ -50,11 +60,10 @@ get_header(); ?><!-- HERO -->
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-8">
-				<p class="lead">Subscribe to our meeting list</p>
+				<p class="lead"><?php echo $optin_text?></p>
 			</div>
 			<div class="col-sm-4">
-				<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">Click
-					here to subscribe</button>
+				<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal"><?php echo $optin_button_text?></button>
 			</div>
 		</div>
 	</div>
